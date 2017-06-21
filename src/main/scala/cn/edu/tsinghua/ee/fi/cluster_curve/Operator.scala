31ff6fc0333fce86fe17bfc00a14850019f7b1a4
Hangyu Fan <fhy14@mails.tsinghua.edu.cn>
2017-06-21 11:05:25 +0800

    debug: Indicator not working.

--- src/main/scala/cn/edu/tsinghua/ee/fi/cluster_curve/Operator.scala
+++ src/main/scala/cn/edu/tsinghua/ee/fi/cluster_curve/Operator.scala
@@ -246,7 +246,8 @@ class Worker(config: Config, testInterval: FiniteDuration, addr2selection: Addre
     }
 
   def indicateProcess(address: Address): Unit = {
-    if ((hb_receive(address) * 100) / mineAmount != ((hb_receive(address) - 1) * 100) / mineAmount)
-      log.info(s"Mining process:$address: ${hb_receive(address) * 100 / mineAmount}")
+    //if ((hb_receive(address) * 100) / mineAmount != ((hb_receive(address) - 1) * 100) / mineAmount)
+      log.info(s"Mining process:$address: ${(hb_receive(address) * 100) / mineAmount}")
+    log.info(s"${hb_receive(address)}")
   }
 }
