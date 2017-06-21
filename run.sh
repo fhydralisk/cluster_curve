72f2a2099fe447ddc460f4e534ff1826e12bd892
Hangyu Fan <fhy14@mails.tsinghua.edu.cn>
2017-06-21 09:34:16 +0800

    run.sh updated

--- run.sh
+++ run.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-MAIN_CLASS="com.nopqzip.App"
-
-if [ -n $1 ]; then
-  [ $1 = "watcher" ] && MAIN_CLASS="com.nopqzip.WatcherApp"
-  [ $1 = "watchee" ] && MAIN_CLASS="com.nopqzip.WatcheeApp"
-fi
+MAIN_CLASS="cn.edu.tsinghua.ee.fi.cluster_curve.CurveApp"
 
 echo "Running $MAIN_CLASS"
 mvn exec:java -Dexec.mainClass=$MAIN_CLASS
